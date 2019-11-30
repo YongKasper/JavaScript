5ef6c143c7da516fbc1ec5a0c50eef0eea832032
YongKasper <nikitoshanskiy@gmail.com>
2019-11-30 18:54:14 +0200

    HW1

---
+++ HomeWork/hw1/js/index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+
+// const adminLogin = "admin";
+// const adminPassword = "m4ngo1zh4ackz0r";
+
+// let a = prompt("login, please:");
+
+// if (a === adminLogin) {
+//   let b = prompt("password, please:");
+//   if (b === adminPassword) {
+//     alert("Добро пожаловать!");
+//   } else if (!b) {
+//     alert("Отменено пользователем!");
+//   } else {
+//     alert("Доступ запрещен, неверный пароль!");
+//   }
+// } else if (!a) {
+//   alert("Отменено пользователем!");
+// } else {
+//   alert("Доступ запрещен, неверный логин!");
+
+// }
+
+// const numberOfSeats = ("Ввести число необходимых мест");
+// if (numberOfSeats <= 6) {
+//     const taba = confirm("we have plase in Taba gruoup")
+//     if (taba){
+//         alert ("Приятного путишествия в группе Taba")
+//     }
+//     else{
+//         alert ("Нам очень жаль, приходите еще!")
+//     }
+   
+// } else if (numberOfSeats > 6 && numberOfSeats <= 15){
+//     const sharm = confirm("we have plase in gruoup");
+//     if  (sharm){
+//         alert("Приятного путишествия в группе Sharm")
+//     }
+//     else{
+//         alert ("Нам очень жаль, приходите еще!")
+//     }
+// }else if (numberOfSeats > 15 && numberOfSeats <= 25 ){
+//     const hurgada = confirm ("we haave plase in gruoup")
+//     if (hurgada){
+//         alert("Приятного путишествия в группе hurgada")
+//     }
+//     else{
+//         alert ("Нам очень жаль, приходите еще!")
+//     }else {
+//         alert("Извините, столько мест нет ни в одной группе!");
+//       }
+//     } else {
+//       alert("Ошибка ввода");
+//     }
+    
